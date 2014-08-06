780ca34abefca117e9ec28fe2558abcb6f12e8da
connor <connor@cs.brown.edu>
2014-08-06 17:06:10 -0400

    Update heatmap code

--- p/heatmap/matrix.js
+++ p/heatmap/matrix.js
@@ -16,19 +16,13 @@ function shuffle(array) {
   return array;
 }
 
+var numAnimals = 8;
+
 var allAnimals = ['BLEE',
-                  'CLAB',
-                  'GORF',
-                  'JUSK',
                   'KWIM',
-                  'LERD',
-                  'MALB',
                   'NEEK',
-                  'PRAH',
                   'RALT',
-                  'SLAH',
                   'SLUB',
-                  'SPAG',
                   'TASP',
                   'VRAY',
                   'WERF'];
@@ -36,7 +30,7 @@ var allAnimals = ['BLEE',
 
 // Generate indecies
 var is = [];
-for(var i = 0; i < 16; i++) {
+for(var i = 0; i < numAnimals; i++) {
   var index = parseInt(Math.random() * allAnimals.length);
   while(is.indexOf(index) > -1) {
     index = parseInt(Math.random() * allAnimals.length);
@@ -45,7 +39,7 @@ for(var i = 0; i < 16; i++) {
 }
 console.log(is);
 
-var animals = Array.apply(null, Array(16)).map(function(_,i) {return allAnimals[is[i]];});
+var animals = Array.apply(null, Array(numAnimals)).map(function(_,i) {return allAnimals[is[i]];});
 console.log(animals);
 console.log('animals.length:', animals.length);
 
@@ -59,7 +53,7 @@ for (a in animals) {
   }
 }
 
-times = Array.apply(null, Array(16)).map(function (_, i) {return (i+4)+':00';});
+times = Array.apply(null, Array(numAnimals)).map(function (_, i) {return (i+4)+':00';});
 
 var data = [];
 
@@ -80,39 +74,87 @@ for (var i = 0; i < minoritySideNum; i++) {
 }
 sideInfo = shuffle(sideInfo);
 
-// Returns a function to generate the sighting values using a Guassian
-function sightings(center, maximum) {
-  var widthOptions = [2,3,4],
+
+// Generate sighting numbers for all animals with Gaussian function
+function generateSightings() {
+  // Returns a function to generate the sighting values using a Guassian
+  function sightings(center, maximum) {
+    var widthOptions = [2,3],
+        widthScalar = Math.random();
+    while (widthScalar <= .5) {
       widthScalar = Math.random();
-  while (widthScalar <= .5) {
-    widthScalar = Math.random();
+    }
+    var base = 0,
+        curveWidth = widthScalar*widthOptions[parseInt(Math.random()*widthOptions.length)]; // for noiser patterns might parameterize this
+    var gaussianFn = function(x){return maximum*Math.exp(-(Math.pow(x-center,2)/(2*Math.pow(curveWidth,2))))+base};
+    return gaussianFn;
   }
-  var base = 0,
-      curveWidth = widthScalar*widthOptions[parseInt(Math.random()*widthOptions.length)]; // for noiser patterns might parameterize this
-  var gaussianFn = function(x){return maximum*Math.exp(-(Math.pow(x-center,2)/(2*Math.pow(curveWidth,2))))+base};
-  return gaussianFn;
+
+  var animalSightings = {};
+  for (a in animals) {
+    var sideOfMostSightings = sideInfo[a];
+
+    var center = sideOfMostSightings < 0 ? times.length/4 : 3*(times.length/4),
+        noiseCenter = sideOfMostSightings > 0 ? times.length/4 : 3*(times.length/4);
+
+    // perturb center
+    center = center + (Math.random() > .5 ? 1 : -1)*1.25*Math.random();
+
+    var sightingFn = sightings(center,1),
+        noiseSightingFn = sightings(noiseCenter, .2);
+
+    var sightingsList = [];
+    for (t in times) {
+      var s = sightingFn(t),
+          sPrime = noiseSightingFn(t),
+          sFinal = s+sPrime > 1 ? 1 : s+sPrime;
+      sightingsList.push(sFinal);
+    }
+
+    var half_length = Math.ceil(sightingsList.length / 2),
+        sightingsListLeft = sightingsList.splice(0,half_length),
+        sightingsListRight = sightingsList;
+
+    sightingsListLeft = shuffle(sightingsListLeft);
+    sightingsListRight = shuffle(sightingsListRight);
+
+    sightingsList = sightingsListLeft.concat(sightingsListRight);
+    animalSightings[a] = sightingsList;
+  }
+
+  return animalSightings;
+} // end generateSightings()
+
+function enoughSightings(animalSightings) {
+  var left = 0,
+      right = 0;
+  for(var key in animalSightings) {
+    var s_right = animalSightings[key].slice(0),// clone the array
+        half_length = Math.ceil(s_right.length /2),
+        s_left = s_right.splice(0,half_length),
+        leftTotal = s_left.reduce(function(a,b){return a+b;}),
+        rightTotal = s_right.reduce(function(a,b){return a+b;});
+    if(leftTotal<rightTotal) {
+      left = left+leftTotal;
+    } else {
+      right = right+rightTotal;
+    }
+  }
+
+  console.log('left/right', left/right);
+  return left/right < .4 && left/right > .1;
 }
 
-for (a in animals) {
-  var sideOfMostSightings = sideInfo[a];
-
-  var center = sideOfMostSightings < 0 ? times.length/4 : 3*(times.length/4),
-      noiseCenter = sideOfMostSightings > 0 ? times.length/4 : 3*(times.length/4);
-
-  // perturb center
-  center = center + (Math.random() > .5 ? 1 : -1)*1.25*Math.random();
-
-  var  sightingFn = sightings(center,1),
-      noiseSightingFn = sightings(noiseCenter, .2);
-
+var animalSightings = generateSightings();
+while(enoughSightings(animalSightings) == false) {
+  animalSightings = generateSightings();
+}
+for (a in animalSightings) {
   for (t in times) {
-    var s = sightingFn(t),
-        sPrime = noiseSightingFn(t),
-        sFinal = s+sPrime > 1 ? 1 : s+sPrime;
-
-    data.push({animal:a, sightings:sFinal, time:t});
+    data.push({animal:a, sightings:animalSightings[a][t], time:t});
   }
 }
+
 
 var sightingVals = data.map(function(d) { return d.sightings;}),
     maxValue = Math.max.apply(null, sightingVals);
@@ -136,25 +178,61 @@ if (maxValueNew > 1) {
 
 
 function addMatrix(colors, container, bgColor) {
-
   // This domain means that the domain [0,1] is the center 80%,
   // meaning that there is 10% contrast to either end of the legend
   // and actdual values
   var color = d3.scale.linear()
       .domain([-.125, 1.125]) // total of 1.25 and 1/1.25 = 0.8
-      .range(colors);
+      .range(colors)
+      .interpolate(d3.interpolateLab);
 
   var svg = container.append('svg')
     .attr('height', 300)
     .attr('width', 300)
     .style('background-color', bgColor)
+    .style('display', 'inline-block')
     .style('padding-top', '20px')
     .style('padding-left', '20px')
     .append('g')
       .attr('transform',('translate(30,0)'));
 
-  var CELL_HEIGHT = 15,
-      CELL_WIDTH = 15,
+  // Add legend
+  var legend = container.append('svg')
+          .attr('height', 50)
+          .attr('width', 330),
+      legendData = [-.125, 0, .125, .25, .375, .5, .625, .75, .875, 1, 1.25];
+
+  legend.selectAll('rect')
+      .data(legendData)
+      .enter()
+        .append('rect')
+            .attr('x', function(d,i) { return i*30; })
+            .attr('y', 0)
+            .attr('width', 30)
+            .attr('height', 30)
+            .attr('fill', function(d){return color(d);})
+            .style('display', 'inline-block');
+  legend.append('svg:path')
+      .attr('d', d3.svg.symbol().type("triangle-up"))
+      .attr('transform', 'translate(165,40)')
+      .style('fill', 'black');
+  legend.append('text')
+      .attr('fill', 'black')
+      .attr('x', 0)
+      .attr('y', 45)
+      .style('font-family', 'sans-serif')
+      .text('0%');
+  legend.append('text')
+      .attr('fill', 'black')
+      .attr('x', 300)
+      .attr('y', 45)
+      .style('font-family', 'sans-serif')
+      .style('text-align', 'right')
+      .text('100%');
+
+
+  var CELL_HEIGHT = 30,
+      CELL_WIDTH = 30,
       VIZ_HEIGHT = CELL_HEIGHT*animals.length,
       VIZ_WIDTH = CELL_WIDTH*times.length;
 
@@ -195,41 +273,41 @@ function addMatrix(colors, container, bgColor) {
             })
             .attr('stroke-width', 1)
             .attr('stroke', bgColor == '#000' ? '#fff' : '#000'),
-      xText = xTextG.selectAll('text')
-          .data(times)
-          .enter()
-            .append('text')
-              .attr('x', function(d,i){return i*CELL_WIDTH + CELL_WIDTH/2})
-              .attr('y', VIZ_HEIGHT+20)
-              .attr('fill', bgColor == '#000' ? '#fff' : '#000')
-              .attr('text-anchor', 'middle')
-              .style('font-family','sans-serif')
-              .style('font-size', '.75em')
-              .text(function(d,i){
-                if (i%4 != 0) {
-                  return '';
-                }
-                return d;
-              }),
+      // xText = xTextG.selectAll('text')
+      //     .data(times)
+      //     .enter()
+      //       .append('text')
+      //         .attr('x', function(d,i){return i*CELL_WIDTH + CELL_WIDTH/2})
+      //         .attr('y', VIZ_HEIGHT+20)
+      //         .attr('fill', bgColor == '#000' ? '#fff' : '#000')
+      //         .attr('text-anchor', 'middle')
+      //         .style('font-family','sans-serif')
+      //         .style('font-size', '.75em')
+      //         .text(function(d,i){
+      //           if (i%4 != 0) {
+      //             return '';
+      //           }
+      //           return d;
+      //         }),
       xTextEarly = xTextG.append('text')
           .attr('x', VIZ_WIDTH/4)
-          .attr('y', VIZ_HEIGHT+35)
+          .attr('y', VIZ_HEIGHT+20)
           .attr('text-anchor', 'middle')
           .style('font-family', 'sans-serif')
-          .style('font-size', '.75em')
+          .style('font-size', '1em')
           .style('fill', bgColor == '#000' ? '#fff' : '#000')
           .text('Early'),
       xTextLate = xTextG.append('text')
           .attr('x', 3*(VIZ_WIDTH/4))
-          .attr('y', VIZ_HEIGHT+35)
+          .attr('y', VIZ_HEIGHT+20)
           .attr('text-anchor', 'middle')
           .style('font-family', 'sans-serif')
-          .style('font-size', '.75em')
+          .style('font-size', '1em')
           .style('fill', bgColor == '#000' ? '#fff' : '#000')
           .text('Late'),
       xTextName = xTextG.append('text')
           .attr('x', VIZ_WIDTH/2)
-          .attr('y', VIZ_HEIGHT+50)
+          .attr('y', VIZ_HEIGHT+35)
           .attr('text-anchor', 'middle')
           .style('font-family', 'sans-serif')
           .style('font-weight', 'bold')
@@ -262,7 +340,7 @@ function addMatrix(colors, container, bgColor) {
               .attr('fill', bgColor == '#000' ? '#fff' : '#000')
               .attr('text-anchor', 'end')
               .style('font-family', 'sans-serif')
-              .style('font-size', '.75em')
+              .style('font-size', '1em')
               .text(function(d){return d;});
 }
 
@@ -306,7 +384,8 @@ function addMatrices(colors) {
     var wrapper = d3.select('#container').append('div'),
       colorInfo = wrapper.append('p').text(colors);
   wrapper.style('border-bottom', '1px solid #ccc')
-      .style('display', 'inline-block');
+      .style('display', 'inline-block')
+      .style('width', '500px');
   addMatrix(colors, wrapper, '#fff');
   addMatrix(colors.reverse(), wrapper, '#fff');
   wrapper.append('br');
