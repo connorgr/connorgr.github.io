aa6da9e801a94eaaa151fb1231408305f4414650
Connor Gramazio <connor@cs.brown.edu>
2017-12-21 22:45:34 -0500

    Update website design

--- img/favicon/favicon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pngquant -- favicon.png
-convert favicon-fs8.png -bordercolor white -border 0 \
-      \( -clone 0 -resize 16x16 \) \
-      \( -clone 0 -resize 128x128 \) \
-      -delete 0 -alpha on -colors 256 favicon.ico
